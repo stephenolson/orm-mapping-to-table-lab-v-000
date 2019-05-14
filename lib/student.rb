9b516ce8ed82f1533c03d464f1e67aa092ad6431
Stephen Olson <stephenolson@gmail.com>
2019-05-14 02:30:26 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -20,9 +20,20 @@ class Student
     DB[:conn].execute(sql)
   end
 
+  def self.drop_table
+    sql =  <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade TEXT
+        )
+        SQL
+    DB[:conn].execute(sql)
+  end
+
   def save
     sql = <<-SQL
-      INSERT INTO songs (name, grade)
+      INSERT INTO students (name, grade)
       VALUES (?, ?)
     SQL
 
