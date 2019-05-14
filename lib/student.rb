abb469a485400451c50d16b7609e74406b929c77
Stephen Olson <stephenolson@gmail.com>
2019-05-14 02:25:16 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,32 +1,32 @@
 class Student
 
   attr_accessor :name, :grade, :id
- 
+
   def initialize(name, grade, id=nil)
     @id = id
     @name = name
     @album = grade
   end
- 
+
   def self.create_table
-    sql =  <<-SQL 
+    sql =  <<-SQL
       CREATE TABLE IF NOT EXISTS songs (
-        id INTEGER PRIMARY KEY, 
-        name TEXT, 
+        id INTEGER PRIMARY KEY,
+        name TEXT,
         grade TEXT
         )
         SQL
-    DB[:conn].execute(sql) 
+    DB[:conn].execute(sql)
   end
- 
+
   def save
     sql = <<-SQL
-      INSERT INTO songs (name, grade) 
+      INSERT INTO songs (name, grade)
       VALUES (?, ?)
     SQL
- 
+
     DB[:conn].execute(sql, self.name, self.grade)
- 
+
   end
-  
+
 end
